d83ac66918c7166976a5db42d5b34d4aa8542f53
tofi1130 <taafuuu@gmail.com>
2024-01-21 09:32:11 +0500

    delete file (no need of it)

--- deploy_cloudflare.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.16.8-stable.tar.xz -o /downloads/flutter.zip
-tar xf /downloads/flutter.zip -C /opt/
-export PATH='$PATH:/opt/flutter/bin'
-pip3 install -r requirments.txt
-flet build web
