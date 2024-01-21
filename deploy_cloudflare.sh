f399af88dceee6586caea07017c6140b270a883b
tofi1130 <taafuuu@gmail.com>
2024-01-21 09:03:09 +0500

    Fix a typo

--- deploy_cloudflare.sh
+++ deploy_cloudflare.sh
@@ -3,5 +3,5 @@
 sudo apt install wget
 wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.16.8-stable.tar.xz -o /downloads/flutter.zip
 tar xf /downloads/flutter.zip -C /opt/
-export $PATH='$PATH:/opt/flutter/bin'
+export PATH='$PATH:/opt/flutter/bin'
 flet build web
