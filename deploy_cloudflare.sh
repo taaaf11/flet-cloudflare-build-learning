f6ea173dcfde9e91c8cfbdc714d13da4c26adf36
tofi1130 <taafuuu@gmail.com>
2024-01-21 09:04:52 +0500

    mkdir

--- deploy_cloudflare.sh
+++ deploy_cloudflare.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-sudo apt install wget
+mkdir /downloads
 wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.16.8-stable.tar.xz -o /downloads/flutter.zip
 tar xf /downloads/flutter.zip -C /opt/
 export PATH='$PATH:/opt/flutter/bin'
