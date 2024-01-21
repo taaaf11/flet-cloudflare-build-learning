daad766693c1654ccc30f7dda106b46f6e8ab4d3
tofi1130 <taafuuu@gmail.com>
2024-01-21 09:17:05 +0500

    remove mkdir

--- deploy_cloudflare.sh
+++ deploy_cloudflare.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 
-sudo mkdir /downloads
 wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.16.8-stable.tar.xz -o /downloads/flutter.zip
 tar xf /downloads/flutter.zip -C /opt/
 export PATH='$PATH:/opt/flutter/bin'
