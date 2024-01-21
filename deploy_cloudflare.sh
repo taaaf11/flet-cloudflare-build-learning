573456788ca4a3024394215ebe02aa80ee77fffa
tofi1130 <taafuuu@gmail.com>
2024-01-21 09:10:50 +0500

    install pkgs

--- deploy_cloudflare.sh
+++ deploy_cloudflare.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-mkdir /downloads
+sudo mkdir /downloads
 wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.16.8-stable.tar.xz -o /downloads/flutter.zip
 tar xf /downloads/flutter.zip -C /opt/
 export PATH='$PATH:/opt/flutter/bin'
-pip install -r requirments.txt
+pip3 install -r requirments.txt
 flet build web
