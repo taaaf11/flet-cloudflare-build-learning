8ed1c6c6611a28a3ea27f02c3654265ca108d5d5
tofi1130 <taafuuu@gmail.com>
2024-01-21 11:03:22 +0500

    checking requests module

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import requests
 import flet as ft
 
 
 def main(page: ft.Page):
-    page.add(ft.Text("This works!"))
+    r = requests.get(requests.get('https://pypi.org/pypi/numpy/json'))
+    page.add(ft.Text(r.content))
 
 
 ft.app(main)
