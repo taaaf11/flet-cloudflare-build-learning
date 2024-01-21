b54b621e290d2f1bcfa2e74b10bf4261930cfc8b
tofi1130 <taafuuu@gmail.com>
2024-01-21 09:01:47 +0500

    Fix flutter download url

--- deploy_cloudflare.sh
+++ deploy_cloudflare.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-sudo apt install wget unzip
-wget https://storage.googleapis.com/flutter_infra_release/releases/stable/windows/flutter_windows_3.16.8-stable.zip -o /downloads/flutter.zip
-unzip /downloads/flutter.zip -d /opt/
-export $PATH='/opt/flutter/bin:$PATH'
+sudo apt install wget
+wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.16.8-stable.tar.xz -o /downloads/flutter.zip
+tar xf /downloads/flutter.zip -C /opt/
+export $PATH='$PATH:/opt/flutter/bin'
 flet build web
